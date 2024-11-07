e7121bf11ea45092d930b961389cf6543b151fb1
nanyanen87 <nan.hanaoka@gmail.com>
2024-11-07 21:53:06 +0900

    comfyUIにリクエスト投げるcontroller作成

---
+++ controllers/comfy_ui_controller.py
@@ -0,0 +1,35 @@
+from flask import request, jsonify
+import json
+import websocket
+
+# confyUIserverにpromptを送信し、imageを取得するcontroller
+def get():
+    # リクエストヘッダのContent-Lengthを取得
+    content_length = int(request.headers['Content-Length'])
+
+    # リクエストボディからデータを取得
+    post_data = request.data
+    json_data = json.loads(post_data)
+    print("Received JSON data:", json_data)  # コンソールに表示
+
+    # websocketでserver_addressに接続し、imagesを返す
+    ws = websocket.WebSocket()
+    ws_url = "ws://{}/ws?clientId={}".format(server_address_ws, client_id)
+    print(f"Connecting to {ws_url}")
+    try:
+        ws = websocket.create_connection(ws_url)
+        print("WebSocket connection established successfully.")
+    except websocket.WebSocketException as e:
+        print(f"WebSocket connection failed: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if 'ws' in locals() and ws.connected:
+            images = self.get_images(ws, json_data)
+            ws.close()
+
+            # imageをoutputImagesディレクトリに保存
+            for node, images in images.items():
+                for i, image in enumerate(images):
+                    with open(f"output/{node}_{client_id}.png", "wb") as f:
+                        f.write(image)
